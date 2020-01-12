760a3063038ec4be5911552b323dee9d935ff0e9
Anton Palgunov <toxblh@gmail.com>
2020-01-12 00:53:43 +0000

    All work on my machine!

--- Program.cs
+++ Program.cs
@@ -1,5 +1,11 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
 namespace MyTrayApp
@@ -9,24 +15,54 @@ namespace MyTrayApp
         [STAThread]
         public static void Main()
         {
+
+            Console.WriteLine("HEY Im HERE!!");
             Application.Run(new SysTrayApp());
+
         }
 
         private NotifyIcon trayIcon;
         private ContextMenu trayMenu;
+        RegistryKey registryKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+        private string appName = "WinToLinux";
+
+        List<string> uefi = new List<string>();
+        List<string> uuid = new List<string>();
+        string bootsequence;
+        string currentValue;
+        int shift;
+
+        Regex regexUUID = new Regex("^(\\{){0,1}[0-9a-fA-F]{8}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{4}\\-[0-9a-fA-F]{12}(\\}){0,1}$");
 
         public SysTrayApp()
         {
+            bool isStart = registryKey.GetValue(appName) != null;
+            getMenuItems();
+
+            currentValue = bootsequence != null ? bootsequence : uuid.First();
+            shift = uuid.Count() - uefi.Count();
+
             // Create a simple tray menu with only one item.
             trayMenu = new ContextMenu();
-            trayMenu.MenuItems.Add("Reboot to...").Enabled = false;
-            trayMenu.MenuItems.Add("-");
-            trayMenu.MenuItems.Add("Windows");
-            trayMenu.MenuItems.Add("Monjaro");
-            trayMenu.MenuItems.Add("-");
             trayMenu.MenuItems.Add("Settings").Enabled = false;
             trayMenu.MenuItems.Add("-");
-            trayMenu.MenuItems.Add("Start with system").Checked = true;
+            trayMenu.MenuItems.Add("Start with system", onRegisterInStartup).Checked = isStart;
+            trayMenu.MenuItems.Add("-");
+            trayMenu.MenuItems.Add("Reboot to...").Enabled = false;
+            trayMenu.MenuItems.Add("-");
+
+            foreach (var pos in uefi.Select((value, i) => new { i, value }))
+                {
+                MenuItem item = new MenuItem();
+                item.Checked = uuid[pos.i + shift] == currentValue;
+                item.Tag = uuid[pos.i + shift];
+                item.Text = pos.value;
+                item.Click += onMenuClick;
+                trayMenu.MenuItems.Add(item);
+            }
+
+            trayMenu.MenuItems.Add("-");
+            trayMenu.MenuItems.Add("Reboot system", onReboot);
             trayMenu.MenuItems.Add("-");
             trayMenu.MenuItems.Add("Exit", OnExit);
 
@@ -49,7 +85,101 @@ namespace MyTrayApp
             Application.Exit();
         }
 
+        private void onMenuClick(object sender, EventArgs e)
+        {
+            string UUID = (sender as MenuItem).Tag.ToString();
+            string command = "/C bcdedit.exe /set {fwbootmgr} bootsequence " + UUID + " /addfirst";
+            Console.WriteLine(command);
 
+            launchCMD(command);
+
+            uuid = new List<string>();
+            uefi = new List<string>();
+
+            launchCMD("/C bcdedit /enum firmware");
+            
+            currentValue = bootsequence != null ? bootsequence : uuid.First();
+            shift = uuid.Count() - uefi.Count();
+            foreach (var pos in uefi.Select((value, i) => new { i, value }))
+            {
+                trayMenu.MenuItems[pos.i + 6].Checked = uuid[pos.i + shift] == currentValue;
+            }
+        }
+
+        private void onRegisterInStartup(object sender, EventArgs e)
+        {
+            bool isStartup = registryKey.GetValue(appName) == null;
+
+            if (isStartup)
+            {
+                registryKey.SetValue(appName, Application.ExecutablePath);
+                trayMenu.MenuItems[2].Checked = true;
+            }
+            else
+            {
+                registryKey.DeleteValue(appName);
+                trayMenu.MenuItems[2].Checked = false;
+            }
+        }
+
+        private void onReboot(object sender, EventArgs e)
+        {
+            launchCMD("/C shutdown /r /t 0");
+        }
+
+        private void getMenuItems()
+        {
+            launchCMD("/C bcdedit /enum firmware");
+        }
+
+        private void launchCMD(string command)
+        {
+            Process build = new Process();
+            build.StartInfo.Arguments = command;
+            build.StartInfo.FileName = "cmd.exe";
+
+            build.StartInfo.UseShellExecute = false;
+            build.StartInfo.RedirectStandardOutput = true;
+            build.StartInfo.RedirectStandardError = true;
+            build.StartInfo.CreateNoWindow = true;
+            build.ErrorDataReceived += build_ErrorAndDataReceived;
+            build.OutputDataReceived += build_ErrorAndDataReceived;
+            build.EnableRaisingEvents = true;
+            build.Start();
+            build.BeginOutputReadLine();
+            build.BeginErrorReadLine();
+            build.WaitForExit();
+        }
+
+        void build_ErrorAndDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (e.Data != null && e.Data != "")
+            {
+                string strMessage = e.Data;
+                string[] splited = strMessage.Split(' ');
+                splited = splited.Where(address => !string.IsNullOrWhiteSpace(address)).ToArray();
+                Match match = regexUUID.Match(splited.Last());
+
+                if (splited[0] == "description")
+                {
+                    splited = splited.Where(w => w != splited[0]).ToArray();
+                    Console.WriteLine(String.Join(" ",splited));
+                    uefi.Add(String.Join(" ", splited));
+                }
+
+                if (splited[0] == "bootsequence")
+                {
+                    Console.Write(splited.Last());
+                    bootsequence = splited.Last();
+                }
+
+                if (splited[0] != "resumeobject" && splited[0] != "bootsequence" && match.Success || splited.Last() == "{bootmgr}")
+                {
+                    Console.Write(splited.Last());
+                    uuid.Add(splited.Last());
+                }
+            }
+        }
 
 
         // All below for hide a Form
